72f5d6809887f3c59b572e4f3695d8dc9013839f
NadiaSbaa <sbaa.nadia@insat.u-carthage.tn>
2020-04-17 11:14:54 +0000

    AngulaRouterBasics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,41 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import { AppComponent } from './app.component';
+import {MatListModule} from '@angular/material/list';
 import 'hammerjs';
 import { MatGridListModule} from '@angular/material/grid-list';
 import { MatCardModule} from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MenuComponent } from './menu/menu.component';
-import {MatListModule} from '@angular/material/list';
-import { DishService} from './services/dish.service';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+import { DishService} from './services/dish.service';
+
+import {AppRoutingModule} from './app-routing/app-routing.module';
+
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
+    AppRoutingModule,
     MatListModule,
     MatGridListModule,
     MatCardModule,
