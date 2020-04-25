5eeea902a61cbcb769a64ca9283bdd4fe8030028
NadiaSbaa <sbaa.nadia@insat.u-carthage.tn>
2020-04-25 22:16:36 +0000

    custom attribute directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,6 +40,7 @@ import { LeaderService } from './services/leader.service';
 import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
 
 import { baseURL } from './shared/baseurl';
+import { HighlightDirective } from './directives/highlight.directive';
 
 
 @NgModule({
@@ -52,7 +53,8 @@ import { baseURL } from './shared/baseurl';
     AboutComponent,
     HomeComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
